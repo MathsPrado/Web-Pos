c09b2fd1232b3b1b0184b1111d6b7ea4b43c569f
MathsPrado <matheusprado55@gmail.com>
2023-03-12 10:58:34 -0300

    Added Project

---
+++ Student.Web/Model/SolicitacaoProjeto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Student.Web.Model
+{
+    public class SolicitacaoProjeto
+    {
+        public string Titulo { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public bool SemDataInicio { get; set; }
+        public bool SemDataFim { get; set; }
+        public string Orçamento { get; set; }
+        public string Local { get; set; }
+        public List<String> ListaConhecimentos { get; set; }
+        public string Descricao { get; set; }
+
+
+    }
+}
